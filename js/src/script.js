1a25b91ae54d9e01f3398d4ef180cdf17d259a71
alfredo giupponi <psncards789@gmai.com>
2021-06-10 00:57:16 -0300

    Nav,SubMenus y NavMovile

--- js/src/script.js
+++ js/src/script.js
@@ -1,5 +1,27 @@
 $(window).on('load', function() {
     //El Dom está listo para el uso
+    const Burger = document.querySelector('.Burger-Menu')
+    const OpenNavMovile = document.querySelector('.MobileNav')
+    onClickBurgerAnimation = () =>{
+        
+        Burger.children[0].classList.toggle('BarraOneBurger')
+        Burger.children[1].classList.toggle('BarraTwoBurger')
+        Burger.children[2].classList.toggle('BarraThreeBurger')
+        OpenNavMovile.classList.toggle('active')
+    }
+
+    //Animation sub menu Movile
+    const arroySubMenu = document.querySelector('.arrowSubMnu')
+    OpenSubMenu = (event) => {
+        
+        if(event.target.parentNode.nextElementSibling.classList.contains('subnav-content-Mobile')){
+            $(event.target.parentNode.nextElementSibling).toggle(400)
+           event.target.children[0].classList.toggle('active')
+            
+        }
+        
+    }
+
 
 });
 
@@ -8,3 +30,7 @@ $(document).ready(function() {
     //El Dom se cargó correctamente.
 
 });
+
+
+//BurgerAnimation
+
